6dfa96aaa71dab666cf53c0c149e8a48575a668c
HughZurname <craig.degouveia@gmail.com>
2017-12-05 20:13:11 +0000

    mostly working, part way through refactoring

--- index.js
+++ index.js
@@ -1,113 +1,89 @@
-const canvas = document.getElementById("canvas");
-const context = canvas.getContext("2d");
-const WIDTH = canvas.width;
-const HEIGHT = canvas.height;
+const canvas = document.getElementById("canvas")
+const context = canvas.getContext("2d")
+const WIDTH = canvas.width
+const HEIGHT = canvas.height
 
-const resetBtn = document.getElementById("reset");
-const happyBtn = document.getElementById("happy");
-const sadBtn = document.getElementById("sad");
-const angryBtn = document.getElementById("angry");
+const resetBtn = document.getElementById("reset")
+const kawaiiBtn = document.getElementById("kawaii")
+const sadBtn = document.getElementById("sad")
+const oniBtn = document.getElementById("oni")
 
-function dataMap(name) {
-    return name.map(f => f[0].split(""))
+const colours = [
+    "rgb(15,55,105)",
+    "rgb(255,255,255)",
+    "rgba(255,255,255,0)",
+    `hsl(${360 * Math.random()}, 50%, 50%)`,
+    "rgb(255,187,128)",
+    "rgb(30,190,254)",
+    "rgba(255,38,0,1)",
+    "rgb(145,145,145)",
+    "rgb(252,247,0)"
+]
+
+const dataMap = name => name.map(f => f[0].split(""))
+
+const drawCell = (x, y, width, height, c) => {
+    context.fillStyle = c
+    context.fillRect(x, y, width, height)
 }
 
-function drawCell(x, y, width, height, c) {
-    context.fillStyle = c;
-    context.fillRect(x, y, width, height);
-}
+const gridColour = index => colours[index]
 
-function gridColour(int) {
-    if (int == 0) {
-        return "rgb(15,55,105)"
-    }
-    if (int == 1) {
-        return "rgb(255,255,255)"
-    }
-    if (int == 2) {
-        return "rgba(255,255,255,0)"
-    }
-    if (int == 3) {
-        return `hsl(${360 * Math.random()}, 50%, 50%)`
-    }
-    if (int == 4) {
-        return "rgb(255,187,128)"
-    }
-    if (int == 5) {
-        return "rgb(30,190,254)"
-    }
-    if (int == 6) {
-        return "rgba(255,38,0,1)"
-    }
-    if (int == 7) {
-        return "rgb(145,145,145)"
-        // return "rgb(255,64,255)"
-    }
-}
+const drawPart = (context, grid, sx, sy) => (
+    grid.map(
+        (row, i) => row.map(
+            (col, j) => drawCell(j * 2 + sx, i * 2 + sy, 2, 2, gridColour(grid[i][j]))
+        )
+    )
+)
 
-function drawEyes(context, x1, x2, y) {
-    context.fillStyle = "rgb(15,55,105)"
-    context.fillRect(x1, y, 6, 6);
-    context.fillRect(x2, y, 6, 6);
-}
-
-function drawPart(context, grid, sx, sy) {
-    clearPart(context, grid, sx, sy)
-    for (let i = 0; i < grid.length; ++i) {
-        for (let j = 0; j < grid[i].length; ++j) {
-            drawCell(j * 2 + sx, i * 2 + sy, 2, 2, gridColour(grid[i][j]));
-        }
-    }
-}
-
-function clearPart(context, grid, sx, sy) {
-    for (let i = 0; i < grid.length; ++i) {
-        for (let j = 0; j < grid[i].length; ++j) {
-            if (grid[i][j] == 0) {
-                context.clearRect(j * 2 + sx, i * 2 + sy, 2, 2);
-            }
-        }
-    }
-}
+const clearPart = (context, grid, sx, sy) => (
+    grid.map(
+        (row, i) => row.map(
+            (col, j) => grid[i][j] == 0 ? context.clearRect(j * 2 + sx, i * 2 + sy, 2, 2) : false
+        )
+    )
+)
 
 function mouse(evt) {
     console.log(`x:${evt.clientX} y:${evt.clientY}`)
     if ((evt.clientX >= 150 && evt.clientX <= 200) && (evt.clientY >= 130 && evt.clientY <= 170)) {
+        currentFace = sad
         cryAnimation()
     }
-    if ((evt.clientX >= 60 && evt.clientX <= 220) && (evt.clientY >= 190 && evt.clientY <= 240)) {
+    if ((evt.clientX >= 60 && evt.clientX <= 215) && (evt.clientY >= 190 && evt.clientY <= 240)) {
         makeBeard()
-        beardCount += 1
-        if (beardCount > 1) {
-            beardAnimation()
-        }
+        beardCount < 1 ? beardCount += 1 : beardAnimation()
     }
-    let boundingRect = canvas.getBoundingClientRect();
+    if ((evt.clientX >= 60 && evt.clientX <= 215) && (evt.clientY >= 40 && evt.clientY <= 100)) {
+        shadesAnimation()
+    }
 }
 
 
 let requestId
 let hasBeard = false
-let hasHair = false
+let gotShades = false
 let currentFace = neutral
 let beardCount = 0
 let beardY = 190,
     tearY = 165,
-    hairY = -100
+    shadesY = -100
 
-function clearAll() {
-    context.clearRect(0, 0, WIDTH, HEIGHT);
+const clearAll = () => context.clearRect(0, 0, WIDTH, HEIGHT)
+
+const tearLoop = () => tearY >= 197 ? tearY = 165 : tearY += 1
+
+const dropShades = () => {
+    if (shadesY >= 135) {
+        console.log("BONG")
+        stop()
+        shadesY = -100
+    }
+    shadesY += 3
 }
 
-function tearDrop() {
-    tearY >= 197 ? tearY = 165 : tearY += 1
-}
-
-function dropHair() {
-    hairY >= 135 ? stop() : hairY += 3
-}
-
-function growBeard() {
+const growBeard = () => {
     if (beardY >= 240) {
         stop()
         beardY = 190
@@ -116,100 +92,76 @@ function growBeard() {
     beardY += 1
 }
 
-function makeBeard() {
-    clearPart(context, dataMap(beard), 45, 168);
-    drawPart(context, dataMap(beard), 45, 168);
+const makeBeard = () => {
+    clearPart(context, dataMap(beard), 45, 168)
+    drawPart(context, dataMap(beard), 45, 168)
     hasBeard = true
 }
 
-function cryAnimation() {
-    requestId = requestAnimationFrame(cryAnimation);
-    drawPart(context, dataMap(sad), 58, 80);
-    if (hasBeard == true) {
-        makeBeard()
-    } else {
-        drawPart(context, dataMap(chin), 46, 186);
-    }
-    clearPart(context, dataMap(tear), 162, tearY - 1);
-    drawPart(context, dataMap(tear), 162, tearY);
-    tearDrop()
+const cryAnimation = () => {
+    requestId = requestAnimationFrame(cryAnimation)
+    drawPart(context, dataMap(currentFace), 58, 80)
+    hasBeard ? makeBeard() : drawPart(context, dataMap(chin), 46, 186)
+    gotShades ? drawPart(context, dataMap(shades), 69, 135) : console.log("SHADES")
+    clearPart(context, dataMap(tear), 162, tearY - 1)
+    drawPart(context, dataMap(tear), 162, tearY)
+    tearLoop()
 }
 
-function beardAnimation() {
-    requestId = requestAnimationFrame(beardAnimation);
-    drawPart(context, dataMap(extraBeard), 45, beardY);
+const beardAnimation = () => {
+    requestId = requestAnimationFrame(beardAnimation)
+    drawPart(context, dataMap(extraBeard), 45, beardY)
     growBeard()
 }
 
-function hairAnimation() {
-    requestId = requestAnimationFrame(hairAnimation);
-    drawPart(context, dataMap(background), 0, 0);
-    drawPart(context, dataMap(currentFace), 58, 80);
-    if (hasBeard == true) {
-        makeBeard()
-    }
-    drawPart(context, dataMap(deal), 69, hairY)
-    drawPart(context, dataMap(deal), 69, hairY)
-    // clearPart(context, dataMap(mohawk), 46, hairY);
-    // drawPart(context, dataMap(mohawk), 46, hairY);
-    dropHair()
+const shadesAnimation = () => {
+    requestId = requestAnimationFrame(shadesAnimation)
+    drawPart(context, dataMap(background), 0, 0)
+    drawPart(context, dataMap(currentFace), 58, 80)
+    drawPart(context, dataMap(shades), 69, shadesY)
+    hasBeard ? makeBeard() : drawPart(context, dataMap(chin), 46, 186)
+    dropShades()
+    gotShades = true
 }
 
-function stop() {
-    requestId && cancelAnimationFrame(requestId);
-}
+const stop = () => requestId && cancelAnimationFrame(requestId)
 
-happyBtn.addEventListener("click", function () {
+kawaiiBtn.addEventListener("click", function () {
     stop()
     clearAll()
-    drawPart(context, dataMap(background), 0, 0);
-    drawPart(context, dataMap(happy), 58, 80);
-        
-    if (hasBeard == true) {
-        makeBeard()
-    }
-    currentFace = happy
+    drawPart(context, dataMap(background), 0, 0)
+    drawPart(context, dataMap(kawaii), 58, 80)
+    hasBeard ? makeBeard() : currentFace = kawaii
 }, false)
 sadBtn.addEventListener("click", function () {
     stop()
     clearAll()
-    drawPart(context, dataMap(background), 0, 0);
-    drawPart(context, dataMap(sad), 58, 80);
-        
-    if (hasBeard == true) {
-        makeBeard()
-    }
-    currentFace = sad
+    drawPart(context, dataMap(background), 0, 0)
+    drawPart(context, dataMap(sad), 58, 80)
+    hasBeard ? makeBeard() : currentFace = sad
 }, false)
-angryBtn.addEventListener("click", function () {
+oniBtn.addEventListener("click", function () {
     stop()
     clearAll()
-    drawPart(context, dataMap(background), 0, 0);
-    drawPart(context, dataMap(angry), 58, 80);
-        
-    if (hasBeard == true) {
-        makeBeard()
-    }
-    currentFace = angry
+    drawPart(context, dataMap(background), 0, 0)
+    drawPart(context, dataMap(oni), 58, 80)
+    hasBeard ? makeBeard() : currentFace = oni
 }, false)
 resetBtn.addEventListener("click", function () {
     stop()
     clearAll()
-    drawPart(context, dataMap(background), 0, 0);
-    drawPart(context, dataMap(neutral), 58, 80);
-        
-    hasBeard = false;
-    beardCount = 0;
+    drawPart(context, dataMap(background), 0, 0)
+    drawPart(context, dataMap(neutral), 58, 80)
+
+    hasBeard = false
+    gotShades = false
+    beardCount = 0
     currentFace = neutral
 }, false)
 
 canvas.addEventListener("click", mouse)
 
-canvas.addEventListener("click", function () {
-    hairAnimation()
-}, false)
+canvas.addEventListener("click", function () {}, false)
 
-drawPart(context, dataMap(background), 0, 0);
-drawPart(context, dataMap(neutral), 58, 80);
-
-drawEyes(context);
+drawPart(context, dataMap(background), 0, 0)
+drawPart(context, dataMap(neutral), 58, 80)
